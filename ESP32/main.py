5fca207d432e6138bbd22e0c819f07b2680507fd
Juerg Beringer <juerg.beringer@gmail.com>
2020-09-07 21:49:54 -0700

    Preliminary version with DHT22 readout using uasyncio

--- ESP32/main.py
+++ ESP32/main.py
@@ -1,54 +1,80 @@
 """
 Air quality monitor in MicroPython using ESP32
 """
-__author__  = 'Juerg Beringer'
+__author__ = 'Juerg Beringer'
+__version__ = "0.1"
 
 import machine
 import ssd1306
-import utime
-import _thread
+from dht import DHT22
+import uasyncio as asyncio
 
 
 # Global variables
 oled = None
-counter = 0
-
-def testThread():
-    global counter
-    while True:
-        counter += 1
-        utime.sleep_ms(10)
+dht_sensor = None
+current_temp = 0
+current_humidity = 0
+n_measure_temp = 0
+n_display = 0
 
 
-def init():
+def hardware_init():
     """Hardware initialization"""
     i2c = machine.I2C(scl=machine.Pin(15), sda=machine.Pin(4))
     pin = machine.Pin(16, machine.Pin.OUT)
-    pin.value(0) # set GPIO16 low to reset OLED
-    pin.value(1) # while OLED is running, must set GPIO16 in high
+    pin.value(0)  # set GPIO16 low to reset OLED
+    pin.value(1)  # while OLED is running, must set GPIO16 in high
     global oled
     oled = ssd1306.SSD1306_I2C(128, 64, i2c)
     oled.fill(0)
-    oled.text('Air Monitor', 0, 0)
+    oled.text('Air Monitor v%s' % __version__, 0, 0)
+    oled.show()
+    global dht_sensor
+    dht_sensor = DHT22(machine.Pin(17))
+
+
+def oled_write(text, posX, posY):
+    """Write text to OLED display (assuming 8x8 font)"""
+    oled.fill_rect(posX * 8, posY * 8, len(text) * 8, 8, 0)
+    oled.text(text, posX * 8, posY * 8)
     oled.show()
 
 
-def oledWrite(text, posX, posY):
-    """Write and show text on OLED display (assuming 8x8 font)"""
-    oled.fill_rect(posX*8, posY*8, len(text)*8, 8, 0)
-    oled.text(text, posX*8, posY*8)
-    oled.show()
+async def measure_temp():
+    global current_temp
+    global current_humidity
+    global n_measure_temp
+    while True:
+        await asyncio.sleep_ms(2000)
+        dht_sensor.measure()
+        current_temp = dht_sensor.temperature()
+        current_humidity = dht_sensor.humidity()
+        n_measure_temp += 1
+
+
+def set_global_exception():
+    """Global exception handler to abort on unhandled exception"""
+    def handle_exception(loop, context):
+        import sys
+        sys.print_exception(context["exception"])
+        sys.exit()
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handle_exception)
+
+
+async def main():
+    set_global_exception()
+    asyncio.create_task(measure_temp())
+    global n_display
+    while True:
+        oled_write('Temp = %3.1f C' % current_temp,0,3)
+        oled_write('#measure = %5i' % n_measure_temp,0,5)
+        oled_write('#display = %5i' % n_display,0,6)
+        n_display += 1
+        await asyncio.sleep_ms(100)
 
 
 if __name__ == '__main__':
-    init()
-    _thread.start_new_thread(testThread, ())
-
-
-    i = 0
-    while True:
-        oledWrite('i = %5i' % i, 0,1)
-        oledWrite('c = %5i' % counter, 0,2)
-        i += 1
-        oled.show()
-        utime.sleep_ms(500)
+    hardware_init()
+    asyncio.run(main())
